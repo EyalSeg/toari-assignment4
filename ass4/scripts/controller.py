fe6a35b41b75d1a1c72b65a184923bc220d3ab29
Unknown <eyals.code@gmail.com>
2018-02-03 19:24:15 +0200

    Implemented arm manipulation - not working!
    It cannot find a plan.

--- ass4/scripts/controller.py
+++ ass4/scripts/controller.py
@@ -7,6 +7,9 @@ from move_base_msgs.msg import MoveBaseAction
 
 from movement import Movement
 from objectDetector import ObjectDetector
+from arm import Arm
+from poses import Poses
+
 
 class Controller:
     def __init__(self):
@@ -21,6 +24,9 @@ class Controller:
         self.object_detector = ObjectDetector("/object_detection/coordinates")
         self.find_red_object = self.object_detector.find_red_object
 
+        self.arm = Arm()
+        self.poses = Poses()
+
         print("I'm up")
 
         # rospy.spin()
@@ -31,11 +37,19 @@ class Controller:
         rospy.signal_shutdown(0)
         return True
 
+    def move_arm(self, target):
+        print 1
+        target = self.poses.transform_point(target, 'base_footprint')
+
+        print target
+        self.arm.set_pose(target.point)
 
 if __name__ == '__main__':
     try:
         controller = Controller()
 
+        controller.find_red_object()\
+            .then(lambda location_stamped: controller.move_arm(location_stamped))
         #an aync navigation example. PROMISES ARE KEWL
         # controller.move([0.9, -1.8], [0, 0, -0.7, 0.7])\
         #     .then(lambda result: controller.move([-2.6, -3.2], [0, 0, 1, 0.01]))\
